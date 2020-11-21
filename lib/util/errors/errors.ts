8332d73120422c26569b2b0f97e67cfdecb22139
kosmo <kosmoziemia@gmail.com>
2020-11-21 13:34:52 +0100

    minor fixes

--- lib/util/errors/errors.ts
+++ lib/util/errors/errors.ts
@@ -1,10 +1,10 @@
-import { TextChannel, DMChannel, NewsChannel } from 'discord.js';
+import { TextChannel, DMChannel, NewsChannel, ColorResolvable } from 'discord.js';
 import { SafeEmbed } from '../embed/SafeEmbed';
 
 export class LoggedError extends Error { //caught by the unhandled rejection handler and logged in a text channel, !not logged in console!
     public channel?: TextChannel | DMChannel | NewsChannel;
 
-    constructor(channel?: TextChannel | DMChannel | NewsChannel, message?: string, cuteColor?: string) { 
+    constructor(channel?: TextChannel | DMChannel | NewsChannel, message?: string, cuteColor?: ColorResolvable) { 
         super(message);
         this.channel = channel;
         let emb = cuteColor ? new SafeEmbed().setColor(cuteColor).setAuthor(message) 
